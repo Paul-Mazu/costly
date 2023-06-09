e35a81d7bd752995dc727d0acb346fe6990018c2
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-06-09 11:54:50 +0200

    add random secret key

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
